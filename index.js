89e24ebb9bdbc25dbe8ab360c3cc7d8771682179
fruzie99 <vmadhurisha99@gmail.com>
2025-11-14 01:05:27 +0530

    Added login function

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ function signup(userName) {
     }
 }
 
+function login(userName, password) {
+    let users = ["madhurisha", "pranav", "tanmayee", "aarthi"];
+
+    if (!users.includes(userName)) {
+        return "User Not Found, Please Signup";
+    } 
+    else if (password !== "Emp@123") {
+        return "Wrong Password....";
+    } 
+    else {
+        return "Login Sucessfull...";
+    }
+}
+
 // testing
-console.log(signup("madhuri"));   // User Already Regsistered
-console.log(signup("newUser"));   // Signup Sucessfull
+console.log("Signup Tests:");
+console.log(signup("madhurisha"));    // User Already Registered
+console.log(signup("newUser"));       // Signup Successful
+
+console.log("\nLogin Tests:");
+console.log(login("madhurisha", "Emp@123")); //logined
+console.log(login("randomUser", "Emp@123"));  // User Not Found
+console.log(login("pranav", "wrongpass"));    // Wrong Password
